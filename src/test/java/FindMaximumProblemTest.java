1db8dd014d63760a6ae64a8dc56224684d71b651
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-14 19:17:56 +0530

    Added: Given three string as combination and get maximum string

--- src/test/java/FindMaximumProblemTest.java
+++ src/test/java/FindMaximumProblemTest.java
@@ -64,5 +64,11 @@ public class FindMaximumProblemTest {
         String maximumOfString = findMaximum.getMaximum("1", "2", "3");
         Assert.assertEquals((String) "3",maximumOfString);
     }
+
+    @Test
+    public void givenThreeStringsAsCombination_WhenGetMaximum_ShouldReturnTheMaximumString() {
+        String maximumOfString = findMaximum.getMaximum("1", "JITESH", "jitesh");
+        Assert.assertEquals((String) "jitesh",maximumOfString);
+    }
 }
 
