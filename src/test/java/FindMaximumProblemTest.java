7683b2fef73003bff227fcc332984a9675d3934a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-14 19:04:01 +0530

    Added: Given three capital string values and get maximum string

--- src/test/java/FindMaximumProblemTest.java
+++ src/test/java/FindMaximumProblemTest.java
@@ -7,56 +7,56 @@ public class FindMaximumProblemTest {
 
     @Test
     public void givenThreeIntegers_WhenMaximumFirstNumber_ShouldReturnTheMaximum() {
-        Integer maximumOfIntegers = findMaximum.getMaximumOfIntegers(10, 9, 8);
+        Integer maximumOfIntegers = findMaximum.getMaximum(10, 9, 8);
         Assert.assertEquals((Integer) 10,maximumOfIntegers);
     }
 
     @Test
     public void givenThreeIntegers_WhenMaximumSecondNumber_ShouldReturnTheMaximum() {
-        Integer maximumOfIntegers = findMaximum.getMaximumOfIntegers(10, 20, 8);
+        Integer maximumOfIntegers = findMaximum.getMaximum(10, 20, 8);
         Assert.assertEquals((Integer) 20,maximumOfIntegers);
     }
 
     @Test
     public void givenThreeIntegers_WhenMaximumThirdNumber_ShouldReturnTheMaximum() {
-        Integer maximumOfIntegers = findMaximum.getMaximumOfIntegers(10, 20, 30);
+        Integer maximumOfIntegers = findMaximum.getMaximum(10, 20, 30);
         Assert.assertEquals((Integer) 30,maximumOfIntegers);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumFirstNumber_ShouldReturnTheMaximum() {
-        Float maximumOfFloats = findMaximum.getMaximumOfFloats(10.2f, 2.5f, 3.6f);
+        Float maximumOfFloats = findMaximum.getMaximum(10.2f, 2.5f, 3.6f);
         Assert.assertEquals((Float) 10.2f,maximumOfFloats);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumSecondNumber_ShouldReturnTheMaximum() {
-        Float maximumOfFloats = findMaximum.getMaximumOfFloats(10.2f, 20.5f, 3.6f);
+        Float maximumOfFloats = findMaximum.getMaximum(10.2f, 20.5f, 3.6f);
         Assert.assertEquals((Float) 20.5f,maximumOfFloats);
     }
 
     @Test
     public void givenThreeFloats_WhenMaximumThirdNumber_ShouldReturnTheMaximum() {
-        Float maximumOfFloats = findMaximum.getMaximumOfFloats(10.2f, 20.5f, 30.6f);
+        Float maximumOfFloats = findMaximum.getMaximum(10.2f, 20.5f, 30.6f);
         Assert.assertEquals((Float) 30.6f,maximumOfFloats);
     }
 
     @Test
     public void givenThreeStrings_WhenMaximumFirstString_ShouldReturnTheMaximumString() {
-        String maximumOfString = findMaximum.getMaximumOfString("jitesh", "dipesh", "akshay");
+        String maximumOfString = findMaximum.getMaximum("jitesh", "dipesh", "akshay");
         Assert.assertEquals((String) "jitesh",maximumOfString);
     }
 
     @Test
     public void givenThreeStrings_WhenMaximumSecondString_ShouldReturnTheMaximumString() {
-        String maximumOfString = findMaximum.getMaximumOfString("dipesh", "jitesh", "akshay");
+        String maximumOfString = findMaximum.getMaximum("dipesh", "jitesh", "akshay");
         Assert.assertEquals((String) "jitesh",maximumOfString);
     }
 
     @Test
-    public void givenThreeStrings_WhenMaximumThirdString_ShouldReturnTheMaximumString() {
-        String maximumOfString = findMaximum.getMaximumOfString("dipesh", "akshay", "jitesh");
-        Assert.assertEquals((String) "jitesh",maximumOfString);
+    public void givenThreeCapitalStrings_WhenMaximumFirstString_ShouldReturnTheMaximumString() {
+        String maximumOfString = findMaximum.getMaximum("JITESH", "DIPESH", "AKSHAY");
+        Assert.assertEquals((String) "JITESH",maximumOfString);
     }
 }
 
