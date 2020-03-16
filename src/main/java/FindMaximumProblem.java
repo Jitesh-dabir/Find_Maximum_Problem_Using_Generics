69dc19f6497f0aaf045abfd64391182b2908e8a2
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-16 09:49:43 +0530

    Refactor: Change class to generic class

--- src/main/java/FindMaximumProblem.java
+++ src/main/java/FindMaximumProblem.java
@@ -1,7 +1,7 @@
-public class FindMaximumProblem {
+public class FindMaximumProblem <T extends Comparable<T>> {
 
     //GENERIC METHOD THAT RETURN MAXIMUM  BETWEEN THREE VALUES
-    public <T extends Comparable<T>> T getMaximum(T firstValue, T secondValue, T thirdValue) {
+    public T getMaximum(T firstValue, T secondValue, T thirdValue) {
         T maxResult = firstValue;
         if (secondValue.compareTo(maxResult) > 0) {
             maxResult = secondValue;
