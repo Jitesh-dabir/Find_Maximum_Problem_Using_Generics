dcad4722c54b88f6fae80a89b9bbed307fd3f697
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-16 10:44:16 +0530

    Added: Parameter constructor and internal method to get maximum

--- src/main/java/FindMaximumProblem.java
+++ src/main/java/FindMaximumProblem.java
@@ -1,7 +1,23 @@
 public class FindMaximumProblem <T extends Comparable<T>> {
 
+    //VARIABLES
+    T firstValue;
+    T secondValue;
+    T thirdValue;
+
+    //DEFAULT CONSTRUCTOR
+    FindMaximumProblem() {
+    }
+
+    //PARAMETER CONSTRUCTOR
+    FindMaximumProblem(T firstValue, T secondValue, T thirdValue ) {
+        this.firstValue=firstValue;
+        this.secondValue=secondValue;
+        this.thirdValue=thirdValue;
+    }
+
     //GENERIC METHOD THAT RETURN MAXIMUM  BETWEEN THREE VALUES
-    public T getMaximum(T firstValue, T secondValue, T thirdValue) {
+    public  <T extends Comparable<T>>  T getMaximum(T firstValue, T secondValue, T thirdValue) {
         T maxResult = firstValue;
         if (secondValue.compareTo(maxResult) > 0) {
             maxResult = secondValue;
@@ -12,6 +28,10 @@ public class FindMaximumProblem <T extends Comparable<T>> {
         return maxResult;
     }
 
+    //METHOD THAT CALL INTERNAL METHOD
+    public T getMaximum() {
+       return getMaximum(firstValue, secondValue, thirdValue);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to find maximum problem");
     }
